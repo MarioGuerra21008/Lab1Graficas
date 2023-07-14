9c9d41284198113e997cbff8fe0224a12fcaf454
Mario Guerra <maguerramorales@gmail.com>
2023-07-13 20:59:42 -0600

    polígono 1 final

--- src/main.cpp
+++ src/main.cpp
@@ -214,10 +214,10 @@ void fillPolygon(unsigned char* framebuffer, int width, int height, const std::v
 
 void render(unsigned char* framebuffer, int width, int height)
 {
-    Vertex polygonPoints[] = { {165, 380}, {185, 360}, {180, 330}, {207, 345}, {233, 330}, {230, 360}, {250, 380}, {220, 385}, {205, 410}, {193, 383} };
-    std::vector<Vertex> polygonVertices(polygonPoints, polygonPoints + sizeof(polygonPoints) / sizeof(polygonPoints[0]));
-    drawPolygon(framebuffer, width, height, polygonVertices, Color(255, 255, 255));
-    fillPolygon(framebuffer, width, height, polygonVertices, Color(255, 255, 0));
+    Vertex polygon1Points[] = { {165, 380}, {185, 360}, {180, 330}, {207, 345}, {233, 330}, {230, 360}, {250, 380}, {220, 385}, {205, 410}, {193, 383} };
+    std::vector<Vertex> polygon1Vertices(polygon1Points, polygon1Points + sizeof(polygon1Points) / sizeof(polygon1Points[0]));
+    fillPolygon(framebuffer, width, height, polygon1Vertices, Color(255, 255, 0));
+    drawPolygon(framebuffer, width, height, polygon1Vertices, Color(255, 255, 255));
 }
 
 int main()
@@ -243,7 +243,7 @@ int main()
     render(framebuffer, width, height);
 
     // Write the image to a BMP file
-    renderBuffer("polygon1.bmp", width, height, framebuffer);
+    renderBuffer("out.bmp", width, height, framebuffer);
 
     // Clean up the memory
     delete[] framebuffer;
